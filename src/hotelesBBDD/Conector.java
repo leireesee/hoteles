5a583503f952d81808ce8d1e161f93089a69ce7b
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-07 08:43:46 +0100

    conector acabado

--- src/hotelesBBDD/Conector.java
+++ src/hotelesBBDD/Conector.java
@@ -1,5 +1,44 @@
 package hotelesBBDD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conector {
 	
+	protected Connection conexion;
+		
+		public void conectar() {
+			
+			conexion = null;
+	
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				conexion = DriverManager.getConnection("jdbc:mysql://localhost/hoteles", "root", "");
+				System.out.println("funciona");
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				System.out.println("no funciona");
+	
+			}
+		}
+		
+		public Connection getConexion() {
+			return conexion;
+		}
+	
+		public void setConexion(Connection conexion) {
+			this.conexion = conexion;
+		}
+	
+		public void cerrar() {
+			try {
+				conexion.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 }
