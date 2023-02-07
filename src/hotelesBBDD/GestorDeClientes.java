e757f64b89b96c2ba157b639a9a312e5bda30c39
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-07 10:02:09 +0100

    estructura base

--- src/hotelesBBDD/GestorDeClientes.java
+++ src/hotelesBBDD/GestorDeClientes.java
@@ -1,5 +1,48 @@
 package hotelesBBDD;
 
-public class GestorDeClientes {
+import java.util.Scanner;
 
+public class GestorDeClientes {
+	
+	public void run(Scanner scan){
+		
+		int opcionMenu;
+		
+		do {
+			VisorMenu.visualizarMenuCliente();
+			opcionMenu = Integer.parseInt(scan.nextLine());
+			
+			switch (opcionMenu){
+			case VisorMenu.ALTA_CLIENTE: {
+				System.out.println("Alta cliente");
+				break;
+			}
+			
+			case VisorMenu.MODIFICAR_CLIENTE: {
+				System.out.println("Modificar cliente");
+				break;
+			}
+			
+			case VisorMenu.ELIMINAR_CLIENTE: {
+				System.out.println("Eliminar cliente");
+				break;
+			}
+			
+			case VisorMenu.VER_CLIENTES: {
+				System.out.println("Ver clientes");
+				break;
+			}
+			
+			case VisorMenu.SALIR: {
+				System.out.println("Alta cliente");
+				break;
+			}
+			
+			default:
+				System.out.println("ERROR");
+			}
+			
+		} while (opcionMenu != VisorMenu.SALIR);
+	}
+	
 }
