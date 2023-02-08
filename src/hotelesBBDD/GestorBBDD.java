1d15fc6fa8cd19fad71e1cf2ab95af35ff7f46d8
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-08 13:17:04 +0100

    metodo insertar cliente

--- src/hotelesBBDD/GestorBBDD.java
+++ src/hotelesBBDD/GestorBBDD.java
@@ -1,5 +1,30 @@
 package hotelesBBDD;
 
-public class GestorBBDD {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class GestorBBDD extends Conector{
+	
+	public boolean insertarCliente(Cliente cliente) {
+		String insertarCliente = "INSERT INTO clientes VALUES (?, ?, ?, ?, ?)";
+		
+		try {
+			PreparedStatement pst = super.conexion.prepareStatement(insertarCliente);
+			pst.setString(1, cliente.getDni());
+			pst.setString(2, cliente.getNombre());
+			pst.setString(3, cliente.getApellidos());
+			pst.setString(4, cliente.getDireccion());
+			pst.setString(5, cliente.getLocalidad());
+			
+			return pst.execute();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+		
+	}
+	
 }
