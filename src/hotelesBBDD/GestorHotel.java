213c8b5106b0bc7ee14e810cdfbb91594c3c7ec4
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-08 12:59:58 +0100

    estructura basica

--- src/hotelesBBDD/GestorHotel.java
+++ src/hotelesBBDD/GestorHotel.java
@@ -1,5 +1,47 @@
 package hotelesBBDD;
 
+import java.util.Scanner;
+
 public class GestorHotel {
 
+	public void run(Scanner scan) {
+
+		int opcionMenu;
+
+		do {
+			VisorMenu.visualizarMenuHoteles();
+			opcionMenu = Integer.parseInt(scan.nextLine());
+
+			switch (opcionMenu) {
+			case VisorMenu.ANADIR_HOTEL: {
+				System.out.println("Añadir hotel");
+				break;
+			}
+
+			case VisorMenu.ELIMINAR_HOTEL: {
+				System.out.println("Eliminar hotel");
+				break;
+			}
+
+			case VisorMenu.MODIFICAR_HOTEL: {
+				System.out.println("Modificar hotel");
+				break;
+			}
+
+			case VisorMenu.VER_HOTELES: {
+				System.out.println("Ver hoteles");
+				break;
+			}
+
+			case VisorMenu.SALIR: {
+				System.out.println("Salir");
+				break;
+			}
+
+			default:
+				System.out.println("ERROR");
+			}
+
+		} while (opcionMenu != VisorMenu.SALIR);
+	}
 }
