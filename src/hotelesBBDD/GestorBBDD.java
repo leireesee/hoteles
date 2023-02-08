6b6f1a6a2095f9d7ee832552049d62ccea314540
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2023-02-08 14:20:15 +0100

    metodo modificarHabitacion y metodo modificarHotel

--- src/hotelesBBDD/GestorBBDD.java
+++ src/hotelesBBDD/GestorBBDD.java
@@ -1,7 +1,9 @@
 package hotelesBBDD;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class GestorBBDD extends Conector{
 	
@@ -44,5 +46,58 @@ public class GestorBBDD extends Conector{
 		}
 		
 		return false;
+	}
+	
+	public boolean modificarHabitacion(Habitacion habitacion) {
+		
+		String st = "UPDATE habitaciones SET id_hotel=?, numero=?, descripcion=?, precio=? WHERE id=?";
+		
+		try {
+			PreparedStatement pst = super.conexion.prepareStatement(st);
+			
+			
+			pst.setInt(1, habitacion.getId_hotel());
+			pst.setString(2, habitacion.getNumero());
+			pst.setString(3, habitacion.getDescripcion());
+			pst.setDouble(4, habitacion.getPrecio());
+			pst.setInt(5, habitacion.getId());
+			
+			return pst.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	public ArrayList<Habitacion> verHabitaciones() {
+		ArrayList<Habitacion> habitaciones = null;
+		
+		String st = "SELECT * FROM habitaciones";
+		
+		try {
+			PreparedStatement pst = super.conexion.prepareStatement(st);
+			ResultSet rs = pst.executeQuery();
+			
+			habitaciones = new ArrayList<>();
+			
+			while(rs.next()) {
+				Habitacion habitacion = new Habitacion();
+				
+				habitacion.setId(rs.getInt("id"));
+				habitacion.setId_hotel(rs.getInt("id_hotel"));
+				habitacion.setNumero(rs.getString("numero"));
+				habitacion.setDescripcion(rs.getString("descripcion"));
+				habitacion.setPrecio(rs.getDouble("precio"));
+				
+				habitaciones.add(habitacion);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return habitaciones;
 	}
 }
