22f53b0987d84a94751c3de078b6d0151c7889b7
Oier Mayoral <Oier Mayoral@HZ309812.H012108.NET>
2023-02-08 14:01:24 +0100

    metodo insertar habitacion

--- src/hotelesBBDD/GestorBBDD.java
+++ src/hotelesBBDD/GestorBBDD.java
@@ -1,5 +1,30 @@
 package hotelesBBDD;
 
-public class GestorBBDD {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class GestorBBDD extends Conector{
+	
+	/*HABITACIONES*/
+	public boolean insertarHabitacion(Habitacion habitacion) {
+		
+		String st = "INSERT INTO habitaciones VALUES (?,?,?,?,?)";
+		
+		try {
+			PreparedStatement pst = super.conexion.prepareStatement(st);
+			
+			pst.setInt(1, habitacion.getId());
+			pst.setInt(2, habitacion.getId_hotel());
+			pst.setString(3, habitacion.getNumero());
+			pst.setString(4, habitacion.getDescripcion());
+			pst.setDouble(5, habitacion.getPrecio());
+			
+			return pst.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
