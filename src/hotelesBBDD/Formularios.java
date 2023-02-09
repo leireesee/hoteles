e6e063782a51698667ada4888f43862e9818fe49
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-09 12:15:28 +0100

    estructura de la clase

---
+++ src/hotelesBBDD/Formularios.java
@@ -0,0 +1,24 @@
+package hotelesBBDD;
+
+import java.util.Scanner;
+
+public class Formularios {
+	
+	public static void pedirDatosCliente(Scanner scan) {
+		
+	}
+	
+	public static void pedirDatosHotel(Scanner scan) {
+		
+	}
+	
+	
+	public static void pedirDatosHabitacion(Scanner scan) {
+		
+	}
+	
+	public static void pedirDatosReserva(Scanner scan) {
+		
+	}
+	
+}
