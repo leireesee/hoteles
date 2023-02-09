1f955d6bc709e902445be6e394a24f29d705c62c
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-09 12:46:31 +0100

    metodos corregidos y terminados

--- src/hotelesBBDD/Formularios.java
+++ src/hotelesBBDD/Formularios.java
@@ -1,10 +1,15 @@
 package hotelesBBDD;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Formularios {
 	
-	public static void pedirDatosCliente(Scanner scan) {
+	public static Cliente pedirDatosCliente(Scanner scan) {
+		
+		Cliente cliente = new Cliente();
 		
 		System.out.println("Introduce el dni del cliente");
 		String dniIntro = scan.nextLine();
@@ -21,9 +26,19 @@ public class Formularios {
 		System.out.println("Introduce la localidad del cliente");
 		String localidadIntro = scan.nextLine();
 		
+		cliente.setDni(dniIntro);
+		cliente.setNombre(nombreIntro);
+		cliente.setApellidos(apellidoIntro);
+		cliente.setDireccion(direccionIntro);
+		cliente.setLocalidad(localidadIntro);
+		
+		return cliente;
+		
 	}
 	
-	public static void pedirDatosHotel(Scanner scan) {
+	public static Hotel pedirDatosHotel(Scanner scan) {
+		
+		Hotel hotel = new Hotel();
 		
 		System.out.println("Introduce el cif del hotel");
 		String cifIntro = scan.nextLine();
@@ -40,10 +55,20 @@ public class Formularios {
 		System.out.println("Introduce la compañia del hotel");
 		String companiaIntro = scan.nextLine();
 		
+		hotel.setCif(cifIntro);
+		hotel.setNombre(nombreIntro);
+		hotel.setGerente(gerenteIntro);
+		hotel.setEstrellas(estrellasIntro);
+		hotel.setCompania(companiaIntro);
+		
+		return hotel;
+		
 	}
 	
 	
-	public static void pedirDatosHabitacion(Scanner scan) {
+	public static Habitacion pedirDatosHabitacion(Scanner scan) {
+		
+		Habitacion habitacion = new Habitacion();
 		
 		System.out.println("Introduce la id de la habitacion");
 		int idIntro = Integer.parseInt(scan.nextLine());
@@ -60,10 +85,53 @@ public class Formularios {
 		System.out.println("Introduce el precio de la habitacion");
 		double precioIntro = Double.parseDouble(scan.nextLine());
 		
+		habitacion.setId(idIntro);
+		habitacion.setId_hotel(idHotelIntro);
+		habitacion.setNumero(numeroIntro);
+		habitacion.setDescripcion(descripcionIntro);
+		habitacion.setPrecio(precioIntro);
+		
+		return habitacion;
+		
 	}
 	
-	public static void pedirDatosReserva(Scanner scan) {
+	public static Reserva pedirDatosReserva(Scanner scan) {
 		
+		Reserva reserva = new Reserva();
+		
+		System.out.println("Introduce la id de la habitacion");
+		int idHabitacionIntro = Integer.parseInt(scan.nextLine());
+		
+		System.out.println("Introduce el dni del cliente");
+		String dniIntro = scan.nextLine();
+		
+		System.out.println("Introduce la fecha de inicio: ");
+		Date fechaInicio = null;
+		
+		try {
+			fechaInicio = new SimpleDateFormat("dd/MM/yyyy").parse(scan.nextLine());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Introduce la fecha del final: ");
+		Date fechaFin = null;
+		
+		try {
+			fechaFin = new SimpleDateFormat("dd/MM/yyyy").parse(scan.nextLine());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		reserva.setId_habitacion(idHabitacionIntro);
+		reserva.setDni(dniIntro);
+		reserva.setDesde(fechaInicio);
+		reserva.setHasta(fechaFin);
+		
+		
+		return reserva;
 	}
 	
 }
