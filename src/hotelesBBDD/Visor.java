7d2ce4d15363d9dc2cfbf8dd52919bbf665607c1
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-09 12:08:18 +0100

    clase y metodos terminados

--- src/hotelesBBDD/Visor.java
+++ src/hotelesBBDD/Visor.java
@@ -1,17 +1,31 @@
 package hotelesBBDD;
 
+import java.util.ArrayList;
+
 public class Visor {
-	public static void mostrarClientes() {
+	public static void mostrarClientes(ArrayList<Cliente> clientes) {
+		for (Cliente cliente : clientes) {
+			System.out.println(cliente);
+		}
 		
 	}
-	public static void mostrarHoteles() {
+	public static void mostrarHoteles(ArrayList<Hotel> hoteles) {
+		for (Hotel hotel : hoteles) {
+			System.out.println(hotel);
+		}
 		
 	}
 	
-	public static void mostrarHabitaciones() {
+	public static void mostrarHabitaciones(ArrayList<Habitacion> habitaciones) {
+		for (Habitacion habitacion : habitaciones) {
+			System.out.println(habitacion);
+		}
 		
 	}
-	public static void mostrarReservas() {
+	public static void mostrarReservas(ArrayList<Reserva> reservas) {
+		for (Reserva reserva : reservas) {
+			System.out.println(reserva);
+		}
 		
 	}
 }
