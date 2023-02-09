807a67443f21bf9cfa6f281f5be32af18c08526c
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-09 12:15:45 +0100

    pedir datos cliente

--- src/hotelesBBDD/Formularios.java
+++ src/hotelesBBDD/Formularios.java
@@ -6,6 +6,21 @@ public class Formularios {
 	
 	public static void pedirDatosCliente(Scanner scan) {
 		
+		System.out.println("Introduce el dni del cliente");
+		String dniIntro = scan.nextLine();
+		
+		System.out.println("Introduce el nombre del cliente");
+		String nombreIntro = scan.nextLine();
+		
+		System.out.println("Introduce los apellidos del cliente");
+		String apellidoIntro = scan.nextLine();
+		
+		System.out.println("Introduce la direccion del cliente");
+		String direccionIntro = scan.nextLine();
+		
+		System.out.println("Introduce la localidad del cliente");
+		String localidadIntro = scan.nextLine();
+		
 	}
 	
 	public static void pedirDatosHotel(Scanner scan) {
