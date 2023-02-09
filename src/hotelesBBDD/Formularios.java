879600ec1bba265f668ba3ccc46bbc5f5265ec28
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-09 12:18:11 +0100

    pedir datos hotel

--- src/hotelesBBDD/Formularios.java
+++ src/hotelesBBDD/Formularios.java
@@ -25,6 +25,21 @@ public class Formularios {
 	
 	public static void pedirDatosHotel(Scanner scan) {
 		
+		System.out.println("Introduce el cif del hotel");
+		String cifIntro = scan.nextLine();
+		
+		System.out.println("Introduce el nombre del hotel");
+		String nombreIntro = scan.nextLine();
+		
+		System.out.println("Introduce el gerente del hotel");
+		String gerenteIntro = scan.nextLine();
+		
+		System.out.println("Introduce las estrellas del hotel");
+		int estrellasIntro = Integer.parseInt(scan.nextLine());
+		
+		System.out.println("Introduce la compañia del hotel");
+		String companiaIntro = scan.nextLine();
+		
 	}
 	
 	
