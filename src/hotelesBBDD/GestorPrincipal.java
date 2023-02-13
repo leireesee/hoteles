776a4911c5087ddd55ee6c9af8ca9c22ea6e88df
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-13 13:46:50 +0100

    gestor principal completado

--- src/hotelesBBDD/GestorPrincipal.java
+++ src/hotelesBBDD/GestorPrincipal.java
@@ -1,5 +1,45 @@
 package hotelesBBDD;
 
-public class GestorPrincipal {
+import java.util.Scanner;
 
+public class GestorPrincipal {
+	
+	public void run(){
+			
+			Scanner scan = new Scanner(System.in);
+			int opcionMenu;
+			
+			do {
+				VisorMenu.visualizarMenuPrincipal();
+				opcionMenu = Integer.parseInt(scan.nextLine());
+				
+				switch (opcionMenu){
+				case VisorMenu.GESTIONAR_CLIENTES: {
+					new GestorDeClientes().run(scan);
+					break;
+				}
+				
+				case VisorMenu.GESTIONAR_RESERVAS: {
+					new GestionReservas().run(scan);
+					break;
+				}
+				
+				case VisorMenu.GESTIONAR_HOTELES: {
+					new GestorHotel().run(scan);
+					break;
+				}
+				
+				case VisorMenu.SALIR: {
+					System.out.println("Salir");
+					break;
+				}
+				
+				default:
+					System.out.println("ERROR");
+				}
+				
+			} while (opcionMenu != VisorMenu.SALIR);
+			
+	}
+	
 }
