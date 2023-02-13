140bbf790cc6c6388f86ebdada0d325c34b0be2c
Leire Sanchez <Leire Sanchez@HZ114833.H012108.NET>
2023-02-13 13:48:03 +0100

    metodo gestor principal llamado

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import hotelesBBDD.GestorPrincipal;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		new GestorPrincipal().run();
 
 	}
 
